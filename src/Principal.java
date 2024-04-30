1a79f121470025b5eaa47f179b0357fddfe6295a
WilJCPimenta <wiljcpimenta@gmail.com>
2024-04-30 18:48:03 -0300

    screenmatch primeiro projeto

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(7);
+
+
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Média das avaliações: " + meuFilme.pegaMeida());
+    }
+}
